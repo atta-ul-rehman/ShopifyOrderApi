ee44aebee5028bb90bcdd580ec18ccd597c40bfc
Atta <rehmanatta866@gmail.com>
2025-07-06 12:40:11 +0500

    Remove node_modules from repo

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
 import app from './app.js';
-import { PORT } from './config/env.js';
-import connectDB from './config/db.js';
 
-// Connect to database
+// Initialize DB connection once
 connectDB();
 
-const port = PORT || 3000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
-
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
-});
+// Wrap Express app in a serverless handler
+export default async (req, res) => {
+  // This is the Vercel-compatible handler
+  return app(req, res);
+};
